084522d2679b94adaaf5c8ddbf8774359e262031
Paulo Borges <dev.pauloborges@gmail.com>
2025-05-22 16:33:55 -0300

    Estrutura da aplicação - Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,13 @@ const app = fastify()
 // http://localhost:3333/hello
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
-app.listen({
-    port: 3333
-}).then(() => {
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP Server Running')
-})
+  })
