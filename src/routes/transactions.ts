31bb5a2d0d6372617c6b5f045800fba8a6ec8ee6
Paulo Borges <dev.pauloborges@gmail.com>
2025-05-26 17:53:14 -0300

    Implementando rotas - Tipagem no Knex

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,18 +5,18 @@ import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
-    const createTransactionsBodySchema = z.object({
+    const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     })
 
-    const { title, amount, type } = createTransactionsBodySchema.parse(
+    const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
 
     await knex('transactions').insert({
-      id: randomUUID,
+      id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
     })
